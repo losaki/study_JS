2b6f055af8664defba4ad226d333e7daf5f45651
losaki <95681771+losaki@users.noreply.github.com>
2022-07-13 18:45:53 +0900

    スプレッド構文

--- src/index.js
+++ src/index.js
@@ -98,8 +98,44 @@
 // const message4 = `名前は${name}です。年齢は${age}です`;
 // console.log(message4);
 
+// /**
+//  * デフォルト値
+//  */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+// sayHello();
+
 /**
- * デフォルト値
+ * スプレッド構文 ...
  */
-const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
-sayHello();
+// 配列の展開
+// const arr1 = [1,2]
+// // console.log(arr1);
+// // console.log(...arr1)
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1)
+
+// //まとめる
+// const arr2 = [1,2,3,4,5];
+// const [num1, num2, ...arr3] = arr2
+// console.log(num1)
+// console.log(num2)
+// console.log(arr3)
+
+//配列のコピー・結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+//スプレッド構文により、元のarr4には影響を与えない
+const arr6 = [...arr4];
+arr6[0] = 100;
+console.log(arr4);
+console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5]
+// console.log(arr7)
+
+// const arr8 = arr4;
+// arr8[0] = 100
+// console.log(arr4);
