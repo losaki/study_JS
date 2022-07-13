5eaf2261e36bc9acfbca8ae3977503c5acb2b7e5
losaki <95681771+losaki@users.noreply.github.com>
2022-07-13 14:48:45 +0900

    変数 var let const の違い

--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
-import "./styles.css";
+/**
+ * コメントアウト const let
+ */
+// var val1 = "var変数";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// // var変数は上書き可能
+// val1 = "上書き";
+// console.log(val1);
+
+// // 再宣言
+// var val1 = "再宣言！";
+// console.log(val1)
+
+// let val2 = "let変数"
+// console.log(val2)
+
+// //letは上書き可能
+// val2 = "let上書き"
+// console.log(val2)
+
+// //letは再宣言不可
+// let val2 = "let再宣言"
+
+// const val3 = "const変数"
+// console.log(val3)
+
+// //val3 = "上書き"
+
+// const val3 = "再定義"
+
+// const val4 = {
+//   name: "NAME",
+//   age: "secret"
+// };
+// val4.name = "yeeees"
+// val4.address = "Japan"
+// console.log(val4)
+
+// const val5 = ['dog', 'cat'];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5)
